b4c9bf8225d2d160459a2de35d818cde13889142
Bj'z <zbjarchi@gmail.com>
2021-11-05 09:55:39 +0100

    build5

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/Password_validator/'
 })
 
-base : '/Password_Validator/'
